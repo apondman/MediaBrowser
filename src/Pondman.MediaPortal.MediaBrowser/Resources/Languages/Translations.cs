b6c4ce77b7be0f2b5ae3d1a1f42e4ad351e763dc
Armand Pondman <apondman@live.nl>
2013-07-24 16:59:47 +0200

    Updated translations

--- src/Pondman.MediaPortal.MediaBrowser/Resources/Languages/Translations.cs
+++ src/Pondman.MediaPortal.MediaBrowser/Resources/Languages/Translations.cs
@@ -8,6 +8,8 @@
         public string Actor = "Actor";
         public string Actors = "Actors";
 
+        public string Client = "Client";
+
         public string Director = "Director";
         public string Directors = "Directors";
         
@@ -16,13 +18,20 @@
         
         public string Movie = "Movie";
         public string Movies = "Movies";
-       
+
+        public string Server = "Server";
+
         public string TVShow = "TV Show";
         public string TVShows = "TV Shows";
 
         public string Writer = "Writer";
         public string Writers = "Writers";
 
+        public string UserProfileLogin = "Login";
+        public string UserProfileLoginFailed = "Login failed.";
+        public string UserProfileProvidePassword = "Please provide the password for this user.";
+        
+        
         // todo: add more translations
     }
 }
