1fe3ee2680496211792e7b400b4f13b5b1675487
Armand Pondman <apondman@live.nl>
2013-10-09 02:22:21 +0200

    Logger now adapts to MediaPortal log level

--- src/Pondman.MediaPortal/Logger/Log4NetLogger.cs
+++ src/Pondman.MediaPortal/Logger/Log4NetLogger.cs
@@ -2,18 +2,29 @@
 using log4net.Appender;
 using log4net.Layout;
 using log4net.Repository.Hierarchy;
-using MediaPortal.Configuration;
 using System;
 using System.Text;
+using MediaPortal.Configuration;
+using MPProfile = MediaPortal.Profile;
+using MPServices = MediaPortal.Services;
 
 namespace Pondman.MediaPortal
 {
-    public class Log4NetLogger: Pondman.MediaPortal.ILogger
+    public class Log4NetLogger: ILogger
     {
         private readonly ILog Log;
         
         public Log4NetLogger(string name)
         {
+            var loglevel = log4net.Core.Level.All;
+            var mploglevel = (MPServices.Level)Enum.Parse(typeof(MPServices.Level), MPProfile.MPSettings.Instance.GetValueAsString("general", "loglevel", "2"));
+            switch (mploglevel)
+            {
+                case MPServices.Level.Information: loglevel = log4net.Core.Level.Info; break;
+                case MPServices.Level.Warning: loglevel = log4net.Core.Level.Warn; break;
+                case MPServices.Level.Error: loglevel = log4net.Core.Level.Error; break;
+            }
+            
             var hierarchy = (Hierarchy)LogManager.CreateRepository(name);
             var patternLayout = new PatternLayout
             {
@@ -35,7 +46,7 @@ namespace Pondman.MediaPortal
             };
             roller.ActivateOptions();
             hierarchy.Root.AddAppender(roller);
-            hierarchy.Root.Level = log4net.Core.Level.All; // todo: change
+            hierarchy.Root.Level = loglevel;
             hierarchy.Configured = true;
 
             Log = LogManager.GetLogger(name, typeof(Log4NetLogger));
